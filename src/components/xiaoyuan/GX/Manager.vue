51d4e33e3f3dc10c5a13698620730ae4835f494b
bbmovistar <bbmovistar@sina.com>
2016-11-19 11:35:46 +0800

    Add files via upload

--- src/components/xiaoyuan/GX/Manager.vue
+++ src/components/xiaoyuan/GX/Manager.vue
@@ -1,1 +1,94 @@
+<template>
+	<div class="pointManage">
+		<div class="breadcrumb">管理员账号管理<el-button type="primary" @click="addManager"></div>
 
+		<el-select
+			v-model="region"
+			filterable
+			placeholder="请输入高校名称"
+			:loading="loading">
+			<el-option
+				v-for="item in option"
+				:key="item.value"
+				:label="item.label"
+				:value="item.value">	
+			</el-option>	
+		</el-select>
+		
+		<el-button type="primary" @click="getManager">搜索</el-button>
+
+		<el-table
+			v-if="tableData.length > 0"
+      		:data="tableData"
+      		style="width: 100%">
+      		<el-table-column
+      		  type="index"
+    	      label="序号"
+    	      width="80">
+      		</el-table-column>
+      		<el-table-column
+      		  prop="schoolName"
+      		  label="高校名称"
+      		  width="200">
+      		</el-table-column>
+      		<el-table-column
+      		  prop="name"
+      		  label="账号"
+      		  width="200">
+      		</el-table-column>
+      		<el-table-column
+    	      inline-template
+    	      label="操作">
+    	      <span>
+    	    	<el-button type="text" size="small" @click="editorManager(row.name)">修改密码</el-button>
+    	    	<el-button type="text" size="small" @click="deleteManager(row.name)">删除</el-button>
+    	      </span>
+    	    </el-table-column>
+    	</el-table>
+	</div>
+</template>
+
+<script>
+	export default{
+		data() {
+			return {
+				region: '',
+				tableData: [{
+        		  schoolName: '北京大学',
+        		  name: 'hah'
+        		}],
+				option: [],
+				list: [],
+				loading: false,
+				states: ["浙江大学", "杭州师范大学", "上海交大"]
+			}
+		},
+		mounted() {
+			this.option = this.states.map(item => {
+				return {value: item, label: item}
+			})
+		},
+		methods: {
+			getManager() {
+				if(!this.region) {
+					this.$notify.error({
+						title: '错误',
+						message: '未选择高校'
+					})
+				}else{
+					console.log(this.region)
+				}
+			},
+			deleteManager(e) {
+				$.confirm('确定删除此管理员账号？', '提示', {
+					confirmButtonText: '确定',
+					cancelButtonText: '取消',
+					type: 'warning'
+				}).then(() => {
+
+				})
+			}
+
+		}
+	}
+</script>
---
+++ src/components/xiaoyuan/GX/schoolManage.vue
@@ -0,0 +1,167 @@
+<template>
+
+	<div class="schoolManage">
+		<div class="breadcrumb"><el-button type="primary">新增高校</el-button></div>
+		
+		<el-select
+    		v-model="value9"
+    		multiple
+    		filterable
+    		remote
+    		placeholder="请输入关键词"
+    		:remote-method="remoteMethod"
+    		:loading="loading">
+    		<el-option
+    		  v-for="item in options4"
+    		  :key="item.value"
+    		  :label="item.label"
+    		  :value="item.value">
+    		</el-option>
+  		</el-select>
+
+    	<el-table
+    	  :data="tableData"
+    	  border
+    	  style="width: 100%">
+    	  <el-table-column
+    	    type="index"
+    	    label="序号"
+    	    width="80">
+    	  </el-table-column>
+    	  <el-table-column
+    	    :data-mid="this.tableData.mid"
+    	    prop="name"
+    	    label="高校名称"
+    	    width="180">
+    	  </el-table-column>
+    	  <el-table-column
+    	    prop="province"
+    	    label="省份">
+    	  </el-table-column>
+    	  <el-table-column
+    	    prop="city"
+    	    label="城市">
+    	  </el-table-column>
+    	  <!-- <el-table-column
+    	    prop="coStatus"
+    	    label="合作状态">
+    	  </el-table-column> -->
+    	  <!-- <el-table-column
+    	    prop="coDate"
+    	    label="合作日期">
+    	  </el-table-column> -->
+    	  <el-table-column
+    	    prop="campNum"
+    	    label="校区数量">
+    	  </el-table-column>
+    	  <el-table-column
+    	    prop="ptNum"
+    	    label="点位数量">
+    	  </el-table-column>
+    	  <el-table-column
+    	    inline-template
+    	    label="操作"
+    	    width="100">
+    	    <span>
+    	    	<el-button type="text" size="small" @click="editorSchool(row.city)" :data-mid="tableData.dataMid">编辑</el-button>
+    	    	<el-button type="text" size="small" >删除</el-button>
+    	    </span>
+    	  </el-table-column>
+    	</el-table>
+	
+  		<el-pagination
+    	  layout="prev, pager, next"
+    	  :page-size="20"
+    	  :total="12">
+    	</el-pagination>
+	</div>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				tableData: [{
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333,
+        		  mid: 1
+        		}, {
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333
+        		}, {
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333
+        		}, {
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333
+        		},{
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333
+        		}, {
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333
+        		}, {
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333
+        		}, {
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333
+        		}, {
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333
+        		}, {
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333
+        		}, {
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333
+        		}, {
+        		  name: '王小虎',
+        		  province: '上海',
+        		  city: '普陀区',
+        		  campNum: 1,
+        		  ptNum: 200333
+        		} ]
+			}
+		},
+		methods: {
+			editorSchool(a) {
+				console.log(a);
+			}
+		}
+	}
+</script>
