785b0ab77fe4b68e9e8a0c49196062f0a2b90607
bbmovistar <bbmovistar@sina.com>
2016-11-19 11:29:50 +0800

    Add files via upload

---
+++ src/components/common/Bottom.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="footer">
+        <span>客服电话：400 800 9198</span>
+        <span class="copy-right">Zhe Jiang Wan Hang Information Technology Co.Ltd</span>
+    </div>
+</template>
+
+<style>
+	#footer{
+        text-indent: 34px;
+    	height: 40px;
+    	line-height: 40px;
+    	font-size: 12px;
+    	color: #c0ccda;
+    	background-color: #324057;
+	}
+	.copy-right {
+    	float: right;
+	}
+</style>
--- src/components/common/Top.vue
+++ src/components/common/Top.vue
@@ -1,1 +1,57 @@
+<template>
+	<div id="top">
+		<el-menu  theme="dark" default-active="1" class="el-menu-demo test" mode="horizontal">
+  			<span>运动世界</span>
+			
+			<el-dropdown  v-if="user.name" @command="loginOut">
+      			<span class="el-dropdown-link">
+        			{{user.name}}欢迎您<i class="el-icon-caret-bottom el-icon--right"></i>
+      			</span>
+      			<el-dropdown-menu slot="dropdown" @command="loginOut">
+        			<el-dropdown-item>退出登录</el-dropdown-item>
+      			</el-dropdown-menu>
+    		</el-dropdown>
 
+			<el-menu-item index="2" v-if="user.name"><router-link :to="'/yunying'">运营管理<router-link></el-menu-item>
+			<el-menu-item index="1" v-if="user.name"><router-link :to="'/xiaoyuan'">校园管理<router-link></el-menu-item>
+		</el-menu>
+	</div>
+</template>
+
+<script>
+ import { mapState } from 'vuex'
+ import { mapActions } from 'vuex'
+	export default {
+		computed: mapState({user: state => state.user}),
+		methods: {
+			...mapActions(['SIGNOUT']),
+			loginOut() {
+				this.SIGNOUT()
+				const router = this.$router
+				router.replace({path: '/login'})
+			}
+		}
+		
+		
+	}
+</script>
+
+<style>
+	#top .admin{
+		float: right;
+	}
+	#top .el-menu-item{
+		float: right;
+	}
+	#top span{
+		color: #c0ccda;
+		line-height: 60px;
+		padding-left: 15px;
+	}
+	#top .is-active a{
+		color: #20a0ff;
+	}
+	#top .el-dropdown{
+		float: right;
+	}
+</style>
